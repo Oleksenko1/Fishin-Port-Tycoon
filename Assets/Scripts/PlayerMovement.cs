a6e8691942d5ccd678dda5ba729f067bf28dcccf
Bogdan Oleksenko <bogdanoleksenko2004@gmail.com>
2025-05-09 23:46:03 +0300

    Init commit. Add basic player movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Stats")]
+    [SerializeField] private float moveSpeed;
+    [Space(5)]
+    [SerializeField] private float rotationSpeed;
+    [SerializeField] private float rotationSmoothness;
+    [Header("Components")]
+    [SerializeField] private FloatingJoystick joystick;
+    [SerializeField] private Transform modelTransform;
+
+    private CharacterController characterController;
+
+    private Quaternion rotationOffset = Quaternion.Euler(0, -45, 0);
+    private Vector2 moveDirection = new Vector2();
+    private float currentAngle = 0f;
+    private float angleVelocity;
+    void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+
+        modelTransform.rotation = rotationOffset;
+    }
+
+    void Update()
+    {
+        moveDirection = joystick.Direction.normalized;
+
+        if (moveDirection != Vector2.zero)
+        {
+            HandleMovement();
+
+            HandleRotation();
+        }
+    }
+    private void HandleMovement()
+    {
+        // Calculating movement vector and multiplying by rotation offset
+        Vector3 movementVector = rotationOffset * new Vector3(moveDirection.x, 0, moveDirection.y);
+
+        characterController.Move(movementVector * Time.deltaTime * moveSpeed);
+    }
+    private void HandleRotation()
+    {
+        float targetAngle = Mathf.Atan2(moveDirection.x, moveDirection.y) * Mathf.Rad2Deg;
+
+        currentAngle = Mathf.SmoothDampAngle(currentAngle, targetAngle, ref angleVelocity, rotationSmoothness);
+
+        modelTransform.rotation = Quaternion.Euler(0, currentAngle, 0) * rotationOffset;
+    }
+
+}
