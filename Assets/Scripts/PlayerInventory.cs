3d5bf150dfab6253ba9cc4084600d036423332f9
Bogdan Oleksenko <bogdanoleksenko2004@gmail.com>
2025-05-13 18:15:19 +0300

    Refactor PlayerInventory script and add AddFish method

--- Assets/Scripts/PlayerInventory.cs
+++ Assets/Scripts/PlayerInventory.cs
@@ -19,31 +19,39 @@ public class PlayerInventory : MonoBehaviour
         {
             for (int i = 0; i < startItems.Count; i++)
             {
-                var fish = Instantiate(startItems[i].fishModel, inventoryTransform);
-                var fishItem = fish.AddComponent<FishItem>();
-                fishItem.fishSO = startItems[i];
-
-                // Configuring Y position
-                if (items.Count > 0)
-                {
-                    var lastFish = items[items.Count - 1];
-                    Vector3 lastFishPos = lastFish.transform.localPosition;
-
-                    float heightOffset = lastFish.fishSO.width / 2 + fishItem.fishSO.width / 2;
-                    Vector3 newFishPos = lastFishPos + Vector3.up * heightOffset;
-
-                    fish.transform.localPosition = newFishPos;
-                }
-                else
-                {
-                    fish.transform.localPosition = Vector3.up * fishItem.fishSO.width / 2;
-                }
-
-                fish.transform.Rotate(Vector3.up * -90);
-                fish.transform.Rotate(Vector3.forward * 90);
-
-                items.Add(fishItem);
+                AddFish(startItems[i]);
             }
         }
+    }
+    public void AddFish(FishSO fishSO)
+    {
+        var fish = Instantiate(fishSO.fishModel, inventoryTransform);
+        var fishItem = fish.AddComponent<FishItem>();
+        fishItem.fishSO = fishSO;
+
+        // Configuring Y position
+        if (items.Count > 0)
+        {
+            var lastFish = items[items.Count - 1];
+            Vector3 lastFishPos = lastFish.transform.localPosition;
+
+            float heightOffset = lastFish.fishSO.width / 2 + fishItem.fishSO.width / 2;
+            Vector3 newFishPos = lastFishPos + Vector3.up * heightOffset;
+
+            fish.transform.localPosition = newFishPos;
+        }
+        else
+        {
+            fish.transform.localPosition = Vector3.up * fishItem.fishSO.width / 2;
+        }
+
+        fish.transform.Rotate(Vector3.up * -90);
+        fish.transform.Rotate(Vector3.forward * 90);
+
+        items.Add(fishItem);
+    }
+    public FishItem TakeFish()
+    {
+        return null;
     }
 }
