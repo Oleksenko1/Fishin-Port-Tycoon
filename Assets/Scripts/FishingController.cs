9add9cd8168b54810aec1e8509c692ad8739c002
Bogdan Oleksenko <bogdanoleksenko2004@gmail.com>
2025-05-17 14:23:48 +0300

    Install vContainer

--- Assets/Scripts/FishingController.cs
+++ Assets/Scripts/FishingController.cs
@@ -54,7 +54,7 @@ public class FishingController : MonoBehaviour
         if (isSuccess)
         {
             playerInventory.AddFish(currentFish);
-            
+
             catchedFishUI.OpenUI(currentFish);
         }
 
